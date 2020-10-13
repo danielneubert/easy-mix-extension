dc6c69d5e0a3f1f4ed137f089b46f1deb5474b0d
Daniel Neubert <git@danielneubert.com>
2020-10-13 18:25:43 +0200

    Remove vue-3-support (fixed with Laravel Mix 6 Beta 9)

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-    'vue-3-support':        require('./vue-3-support'),
     'babel-root-import':    require('./babel-root-import'),
     'babel-dynamic-import': require('./babel-dynamic-import'),
 }
